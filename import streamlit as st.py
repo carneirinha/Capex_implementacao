b7983e6fe151374c9f3e707005a9494bb286294c
carneirinha <146026710+carneirinha@users.noreply.github.com>
2024-08-12 11:06:57 -0300

    Add files via upload

---
+++ import streamlit as st.py
@@ -0,0 +1,33 @@
+import streamlit as st
+st.write("Hello ,let's learn how to build a streamlit app together")
+import streamlit as st
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+import streamlit as st
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick your gender',['Male','Female'])
+st.selectbox('Pick your gender',['Male','Female'])
+st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0,50)
+import streamlit as st
+st.number_input('Pick a number', 0,10)
+st.text_input('Email address')
+st.date_input('Travelling date')
+st.time_input('School time')
+st.text_area('Description')
+st.file_uploader('Upload a photo')
+st.color_picker('Choose your favorite color')
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
